2ee33ddf47dbc8ea79b282614a5f15675531e6f4
eprj453 <eprj453@gmail.com>
2019-11-25 00:26:15 +0900

    191125 0026 | sangwon

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,4 @@ from . import views
 app_name='movies'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get_movieData/', views.get_movieData, name="get_movieData"),
-    path('get_genreData/', views.get_genreData, name='get_genreData'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -15,37 +15,3 @@ def index(request):
     context = { 'messages': messages }
     return render(request, 'movies/index.html', context)
 
-def get_movieData(request):
-    api_key =  config('API_KEY')
-    url = f'https://api.themoviedb.org/3/discover/movie?api_key={api_key}&primary_release_year=2017&language=ko-KR&sort_by=revenue.desc'
-    api_data = requests.get(url).json()
-    # pprint(api_data)
-    movies = api_data.get('results')
-    # pprint(movies)
-    for movie in movies:
-        empty_movie = Movie()
-        empty_movie.title = movie.get('title')
-        empty_movie.original_title = movie.get('original_title')
-        empty_movie.summary = movie.get('overview')
-        empty_movie.come_out = movie.get('release_date')
-        empty_movie.poster_url = 'https://image.tmdb.org/t/p/original/' + movie.get('poster_path')
-        empty_movie.backdrop_url = 'https://image.tmdb.org/t/p/original/' + movie.get('backdrop_path')
-        empty_movie.character_id = movie.get('id')
-        get_genres = movie.get('genre_ids')
-
-        empty_movie.save()
-        for g in get_genres:
-            empty_movie.genres.add(g)
-    return redirect('movies:index')
-
-def get_genreData(request):
-    url = f'https://api.themoviedb.org/3/genre/movie/list?api_key={api_key}&language=ko-KR'
-    genre_data = requests.get(url).json()
-    genres = genre_data.get('genres')
-    for genre in genres:
-        t_genre = Genre()
-        t_genre.id = genre.get('id')
-        t_genre.name = genre.get('name')
-        t_genre.save()
-    # print(genres)
-    return redirect('movies:index')
