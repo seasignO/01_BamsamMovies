c4895e68885523d360e339379906298d1baca962
eprj453 <eprj453@gmail.com>
2019-11-26 17:20:00 +0900

    191126 1719 | sangwon

--- movies/urls.py
+++ movies/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('rating_modify/<int:movie_pk>/<int:rating_pk>/', views.rating_modify, name="rating_modify"),
     path('like/<int:movie_pk>/', views.like, name='like'),
     path('main/', views.main, name='main'),
+    path('addRating/<int:movie_pk>/', views.addRating, name='addRating'),
 
 ]
